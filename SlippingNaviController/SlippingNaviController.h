dcd39ca39290bccbc05faa57c5612187a20516a1
Dev.MJ <mr.lucifers@gmail.com>
2017-05-07 22:53:55 +0900

    - make project

---
+++ SlippingNaviController/SlippingNaviController.h
@@ -0,0 +1,19 @@
+//
+//  SlippingNaviController.h
+//  SlippingNaviController
+//
+//  Created by Dev.MJ on 07/05/2017.
+//  Copyright © 2017 Dev.MJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlippingNaviController.
+FOUNDATION_EXPORT double SlippingNaviControllerVersionNumber;
+
+//! Project version string for SlippingNaviController.
+FOUNDATION_EXPORT const unsigned char SlippingNaviControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlippingNaviController/PublicHeader.h>
+
+
